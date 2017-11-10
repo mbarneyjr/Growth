9cfdc1306125be796d071c6f07f998aef0c73520
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2017-11-10 01:33:52 -0500

    adds platform, player, and light and camera controls

---
+++ Growth/Assets/Scripts/LightController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+
+    public float dayLength;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationSpeed = Time.deltaTime / dayLength;
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
---
+++ Growth/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        Respawn();
+    }
+
+    public void Respawn()
+    {
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+        gameObject.GetComponent<Renderer>().material.color = new Vector4(UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), 1.0f);
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        transform.position = new Vector3(0.0f, 1.0f, 0.0f);
+    }
+
+    private void AddSize(float otherRadius)
+    {
+        double otherVolume = 4.0f * Math.PI * Math.Pow(otherRadius, 3) / 3;
+        float myRadius = gameObject.transform.localScale.x;
+        double myVolume = 4.0f * Math.PI * Math.Pow(myRadius, 3) / 3;
+        double myNewVolume = myVolume + otherVolume;
+        double myNewRadius = Math.Pow((3 * myNewVolume) / (4 * Math.PI), 1.0 / 3.0);
+        gameObject.transform.localScale = new Vector3((float)myNewRadius, (float)myNewRadius, (float)myNewRadius);
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
